fe6703c4f0247d9ae4a550d86ad7e3885a8afa48
Hannahphoto <hannahprice67@gmail.com>
2023-10-08 13:26:00 -0400

    javascript prompt

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var generatePassword = function (){
+  window.prompt("How many characters will your password contain?")
+  if (!prompt)
+  return;
+  window.prompt("Your password will contain numbers?")
+  if (!prompt)
+  return;
+  window.prompt("Your password will contain a lowercase letter?")
+  if (!prompt)
+  return;
+  window.prompt("Your password will contain an uppercase letter?")
+  if (!prompt)
+  return
+  window.prompt("Your password will contain a special character?")
+  if(!prompt)
+  return
+  else (generatePassword)
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -7,6 +25,14 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  for (var generatePassword){
+    [Math.floor(Math.random)];
+  }
+  
+  if (writePassword == generatePassword)
+  
+  return(password);
 
 }
 
